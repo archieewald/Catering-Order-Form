4ef91935759000b504fd830890a996960bd5d864
Archieewald <Arkadiuszdabek@gmail.com>
2018-02-21 12:49:34 +0100

    Validation on Personal Data

--- js/main_form.jsx
+++ js/main_form.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Moment from 'moment';
 import momentLocalizer from 'react-widgets-moment';
 import DateTimePicker from 'react-widgets/lib/DateTimePicker';
+import simpleNumberLocalizer from 'react-widgets-simple-number';
+import NumberPicker from 'react-widgets/lib/NumberPicker';
 
 
 import OrderDetails from './order_details.jsx';
@@ -14,27 +16,118 @@ class MainForm extends React.Component{
         super(props);
         Moment.locale('pl');
         momentLocalizer();
+        simpleNumberLocalizer();
         this.state = {
-            form: []
+            name: '',
+            phone: '',
+            email: '',
+            time: '',
+            deliveryR: '',
+            address: '',
+            request: '',
+            visName: 'none',
+            visEmail: 'none',
+            visPhone: 'none',
+            visDelivery: 'none',
+            visForm: false,
+            visAddress: 'none',
+            visTime: 'none',
+            visDate: 'block'
         }
     }
-    getPersonalDetails = (x) => {
-        const formData = [x];
-        console.log(formData);
+    onTypeText = (element) => {
+        this.setState({
+            [element.id]: element.value
+        });
+
+    }
+    onTypeCheck = (element) => {
+        this.setState({
+            [element.id]: element.checked
+        })
+    }
+    onTypeRadio = (element) => {
+        this.setState({
+            deliveryR: element.value
+        })
+    }
+    personalDetailsValidation(){
+        this.setState({
+            visForm: "none",
+            visPhone: "none",
+            visName: "none",
+            visEmail: "none",
+            visTime: 'none',
+            visDelivery: 'none',
+            visAddress: 'none'
+        });
+        let isEmailValid = this.state.email.indexOf('@') > -1;
+        let isNameValid = this.state.name !== "";
+        let isPhoneValid = this.state.phone !== "";
+        let isAddressValid = this.state.address !== "";
+        let isTimeValid = this.state.time !== "";
+        let isDeliveryValid = this.state.deliveryR !== '';
+
+        if(!isEmailValid){
+            this.setState({
+                visEmail: "block",
+            })
+        }
+        if (!isNameValid){
+            this.setState({
+                visName: "block",
+            })
+        }
+        if (!isPhoneValid){
+            this.setState({
+                visPhone: "block",
+            })
+        }
+        if (!isTimeValid){
+            this.setState({
+                visTime: "block",
+            })
+        }
+        if (!isDeliveryValid){
+            this.setState({
+                visDelivery: "block",
+            })
+        }
+        if (!isAddressValid){
+            this.setState({
+                visAddress: "block",
+            })
+        }
+
+        if(isPhoneValid && isNameValid && isEmailValid && isAddressValid && isTimeValid && isDeliveryValid){
+            this.setState({
+                visForm: true
+            })
+
+        }
+
     }
     submitForm = (event) => {
+        event.preventDefault();
         //filter getPersonalDetails z znaczników dla walidacji
+        this.personalDetailsValidation();
     }
     render(){
         return(
-            <div className='container'>
-                <OrderDetails getDetails={this.getPersonalDetails}/>
-            </div>
+            <form className='form'>
+                <div className='container'>
+                    <OrderDetails name={this.state.name} phone={this.state.phone} email={this.state.email} deliveryR={this.state.deliveryR}
+                                  address={this.state.address} time={this.props.time} request={this.state.request} visName={this.state.visName}
+                                  visEmail={this.state.visEmail} visPhone={this.state.visPhone} visDelivery={this.state.visDelivery} visAddress={this.state.visAddress}
+                                  visTime={this.state.visTime} onChangeText={this.onTypeText} onChangeCheck={this.onTypeCheck} onChangeRadio={this.onTypeRadio}
+                                  onSubmit={this.submitForm}/>
+                </div>
+                <input type='submit' value='Zamawiam' onClick={this.submitForm}/>
+            </form>
 
         )
     }
 }
-
 
 document.addEventListener('DOMContentLoaded', function(){
     ReactDOM.render(
--- js/order_details.jsx
+++ js/order_details.jsx
@@ -3,130 +3,88 @@ import ReactDOM from 'react-dom';
 import Moment from 'moment';
 import momentLocalizer from 'react-widgets-moment';
 import DateTimePicker from 'react-widgets/lib/DateTimePicker';
+import simpleNumberLocalizer from 'react-widgets-simple-number';
+import NumberPicker from 'react-widgets/lib/NumberPicker';
+
 
 
 export default class OrderDetails extends React.Component{
-    constructor(props){
+    constructor(props) {
         super(props);
         Moment.locale('pl');
         momentLocalizer();
-        this.state = {
-            name: '',
-            phone: '',
-            email: '',
-            delivery: false,
-            pickUp: false,
-            address: '',
-            request: '',
-            visName: 'block',
-            visEmail: 'block',
-            visPhone: 'block',
-            visDelivery: 'block',
-        }
+        simpleNumberLocalizer();
     }
-    onTypeText = (event) => {
-        this.setState({
-            [event.target.id]: event.target.value
-        });
-
-        this.setState({
-            visForm: "none",
-            visMess: "none",
-            visName: "none",
-            visEmail: "none"
-        });
-
-        let isEmailValid = this.state.email.indexOf('@') > -1;
-        let isNameValid = this.state.name !== "";
-        let isPhoneValid = this.state.message !== "";
-        let isAddressValid = this.state.address !== '';
-
-        if(!isEmailValid){
-            this.setState({
-                visEmail: "block",
-            })
-        }
-        if (!isNameValid){
-            this.setState({
-                visName: "block",
-            })
-        }
-        if (!isPhoneValid){
-            this.setState({
-                visPhone: "block",
-            })
-        }
-        if(isPhoneValid && isNameValid && isEmailValid && isAddressValid){
-            this.setState({
-                visForm: "block",
-            })
-
-        }
+    handleChangeText= (event) => {
+        this.props.onChangeText(event.target);
     }
-    onTypeCheck = (event) => {
-        this.setState({
-            [event.target.id]: event.target.checked
-        })
+    handleChangeCheck= (event) => {
+        this.props.onChangeCheck(event.target);
     }
-    componentDidUpdate(){
-        const personalDetails = this.state;
-        this.props.getDetails(personalDetails);
+    handleChangeRadio= (event) => {
+        this.props.onChangeRadio(event.target);
     }
     render(){
         return(
-            <form className='order_details'>
+            <div className='order_details'>
                 <h2 className='order_details_title'>1) Dane kontaktowe, szczegóły dostawy oraz uwagi</h2>
                 <hr />
-                <div className='form_accepted'>
-                    <span style={{color: "green", display: this.state.visForm}}>Dziękujemy za wiadomość. Wysłano do wydziału: {this.state.department}.</span>
-                </div>
+                 {/*okienko walidacji wyskakuje dla wszystkich formularzy*/}
                 <div className='name_form'>
                     <label className='label' htmlFor='name'>Imię i nazwisko<span className='required'>*</span></label>
-                    <span style={{color: "red", display: this.state.visName}}>Pole imię musi zostać uzupełnione</span>
-                    <input id='name' type='text' value={this.state.name} onChange={this.onTypeText}/>
+                    <span style={{color: "red", display: this.props.visName}}>Pole imię musi zostać uzupełnione</span>
+                    <input id='name' type='text' value={this.props.name} onChange={this.handleChangeText}/>
                     <p className='descrInput'>*lub nazwa firmy</p>
                 </div>
                 <div className='phone_form'>
                     <label className='label' htmlFor='phone'>Numer telefonu<span className='required'>*</span></label>
-                    <span style={{color: "red", display: this.state.visPhone}}>Pole z numerem telefonu musi zostać uzupełnione</span>
-                    <input id='phone' type='text' value={this.state.phone} onChange={this.onTypeText}/>
+                    <span style={{color: "red", display: this.props.visPhone}}>Pole z numerem telefonu musi zostać uzupełnione</span>
+                    <input id='phone' type='text' value={this.props.phone} onChange={this.handleChangeText}/>
                 </div>
                 <div className='email_form'>
                     <label className='label' htmlFor='email'>Email<span className='required'>*</span></label>
-                    <span style={{color: "red", display: this.state.visEmail}}>Pole email musi zostać uzupełnione i zawierać znak @</span>
-                    <input id='email' type='email' value={this.state.email} onChange={this.onTypeText}/>
+                    <span style={{color: "red", display: this.props.visEmail}}>Pole email musi zostać uzupełnione i zawierać znak @</span>
+                    <input id='email' type='email' value={this.props.email} onChange={this.handleChangeText}/>
                 </div>
                 <div className='date_form'>
                     <label className='label' htmlFor='date'>Podaj datę realizacji zamówienia<span className='required'>*</span></label>
-                    <DateTimePicker/>
+                    <span style={{color: "red", display: this.props.visDate}}>Pole z datą musi zostać uzupełnione</span>
+                    <DateTimePicker id='date' time={false}/>
+            {/*//WALIDACJA DATY!!!*/}
+                    <label className='label' htmlFor='time'>Podaj godzinę realizacji zamówienia<span className='required'>*</span></label>
+                    <span style={{color: "red", display: this.props.visTime}}>Pole z godziną musi zostać uzupełnione</span>
+                    <input type={'time'} id='time' value={this.props.time} pattern="[0-9]{2}:[0-9]{2}"  onChange={this.handleChangeText}/>
                     <p className='descrInput'>- zamówienia można składać na minimum 7 dni przed datą realizacji;
                         <br/>- może zdarzyć się, że ze względu na mnogą ilość zamówień w podanym terminie nie będziemy w stanie wykonać dla Państwa usługi - w takim przypadku poinformujemy Państwa telefonicznie w przeciągu 24h;
-                        <br/>- po przyjęciu zamówienia do realizacji otrzymają Państwo wiadomość email z danymi do przelewu na kwotę 10% wartości zamówienia - <strong>uiszczenie wpłaty jest gwarantem wykonania usługi.</strong>
+                        <br/>- po przyjęciu zamówienia do realizacji otrzymają Państwo wiadomość email z podsumowaniem oferty oraz z danymi do przelewu na kwotę 10% wartości zamówienia - <strong>uiszczenie wpłaty jest gwarantem wykonania usługi.</strong>
                     </p>
                 </div>
                 <div className='delivery_option_form'>
                     <label className='label' htmlFor='delivery pickUp '>Odbiór własny lub dowóz<span className='required'>*</span></label>
                     <p className='descrInput'>- koszt dowozu ustalany jest w zależności od trasy do przebycia oraz od kwoty zamówienia.</p>
-                    <span style={{color: "red", display: this.state.visDelivery}}>Proszę zaznaczyć opcję dostawy</span>
+                    <span style={{color: "red", display: this.props.visDelivery}}>Proszę zaznaczyć opcję dostawy</span>
                     <div className='checkbox-container'>
-                        <input id='delivery' type='checkbox' checked={this.state.delivery} onChange={this.onTypeCheck}/>
+                        <input id='delivery' name='deliveryR' type='radio' value='delivery' checked={this.props.deliveryR === 'delivery'} onChange={this.handleChangeRadio}/>
                         <label className='labelCheckbox' htmlFor='delivery'>Dowóz</label>
                     </div>
                     <div className='checkbox-container'>
-                        <input id='pickUp' type='checkbox' checked={this.state.pickUp} onChange={this.onTypeCheck}/>
+                        <input id='pickUp' name='deliveryR' type='radio' value='pickUp' checked={this.props.deliveryR === 'pickUp'} onChange={this.handleChangeRadio}/>
                         <label className='labelCheckbox' htmlFor='pickUp'>Odbiór własny<span className='optional'>  (dodatkowo płatne)</span></label>
                     </div>
                 </div>
                 <div className='address_form'>
                     <label className='label' htmlFor='address'>Adres Dostawy<span className='required'>*</span></label>
-                    <textarea className='textarea_form' id='address' value={this.state.address} onChange={this.onTypeText}/>
+                    <span style={{color: "red", display: this.props.visAddress}}>Proszę podać adres dostawy</span>
+                    <textarea className='textarea_form' id='address' value={this.props.address} onChange={this.handleChangeText}/>
                 </div>
                 <div className='special_requests'>
                     <label className='label' htmlFor='request'>Dodatkowe informacje: <span className='optional'> alergie na składniki, dodatkowy sprzęt serwisowy, komentarze ...</span></label>
-                    <textarea className='textarea_form' id='request' value={this.state.request} onChange={this.onTypeText}/>
+                    <textarea className='textarea_form' id='request' value={this.props.request} onChange={this.handleChangeText}/>
                 </div>
 
-            </form>
+            </div>
         )
     }
 }
+
+
